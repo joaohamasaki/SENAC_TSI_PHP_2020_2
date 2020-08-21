5eeff48d6de217648a2c30dc05f1a96e9f6b4c94
Joao Hamasaki <hamasaki.joao@gmail.com>
2020-08-20 22:11:08 -0300

    Exemplo de operadores rodando já com sv web

---
+++ operadores/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//comentário de linha
+
+/*
+*
+* Comentário de bloco
+*
+*/
+
+echo "Olá mundo!";
+
+$saldo = 34.5;
+
+echo "<br><br><br>Seu saldo é de $saldo";
+
+
+$saldo *=  2;
+
+echo "<br><br><br>Agora seu saldo é de $saldo";
+
+
+$resto = $saldo % 673;
