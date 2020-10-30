6f93e0136defad7c2aa1e82360c6d5cb34b46387
joaohamasaki <hamasaki.joao@gmail.com>
2020-10-29 22:48:05 -0300

    Aprendendo a criar uma tela de login funcional

---
+++ login_db/index_tpl.php
@@ -0,0 +1,8 @@
+<br>
+<center>
+	<a href="/senac-tsi-php-2020-2/login_db/nps_tpl.php">NPS</a> 
+	<a href="/senac-tsi-php-2020-2/login_db/outro.php">Outro</a> 
+	<a href="/senac-tsi-php-2020-2/login_db/qualquer.php">Qualquer</a> 
+	<a href="/senac-tsi-php-2020-2/login_db/novo/umoutroainda.php">Um outro ainda</a> 
+	<a href="/senac-tsi-php-2020-2/login_db/sair.php">Sair</a> 
+</center>
