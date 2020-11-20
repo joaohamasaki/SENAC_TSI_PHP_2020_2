31f018b5281f2f25bb00360fa297baf946bacc6c
joaohamasaki <hamasaki.joao@gmail.com>
2020-11-19 22:31:05 -0300

    Trabalhando com arquivos CRUD com upload de img

--- login_db/usuario/view/editar.php
+++ login_db/usuario/view/editar.php
@@ -1,104 +1,48 @@
-<?php
+<center>
+	<br><br><h1>EDITAR USUÁRIO</h1><br><br>
+	<table border="1">
+		<tr>
+			<td>
+				<form method="post">
+					<br>
+					<?php
+					if ( isset($editado_ok) ) {
+						echo "<font color='green'>Editado com sucesso!</font>";
+					}
+					?>
+					<font color="red">
+					<?php
+					if ( isset($erros) ){
+						if ( count($erros) > 0 ) {
 
-chdir( __DIR__ );
-
-require_once '../../db.php';
-
-function listar( int $id = null): array 
-{
-	global $db;
-
-	if ( is_null($id) ) { 
-
-		$str = '';
-	
-	} else {
-
-        $str = 'WHERE id = ' . preg_replace('/\D/', '', $id);
-        
-	}
-
-	$r = $db->query("SELECT id, nome, email FROM usuario $str");
-	$reg = $r->fetchAll();
-
-	return is_array($reg) ? $reg : [];
-}
-
-function ja_existe_email( string $email ): bool
-{
-	if ( empty($email) ) return false;
-
-	global $db;
-
-	$stmt = $db->prepare('SELECT id FROM usuario WHERE email = :email');
-
-	$stmt->bindParam(':email', $email);					
-
-	$stmt->execute();
-
-	$registro = $stmt->fetch();
-
-	return is_numeric($registro['id']) ? true : false;
-}
-
-function gravar_usuario( string $nome, string $email, string $senha): ?int 
-{
-	global $db;
-
-	$senha = password_hash( $senha, PASSWORD_DEFAULT);
-
-	$stmt = $db->prepare('	INSERT INTO usuario 
-								( nome, email, senha) 
-							VALUES 
-								( :nome, :email, :senha)');
-
-	$stmt->bindParam(':nome', $nome);
-	$stmt->bindParam(':email', $email);
-	$stmt->bindParam(':senha', $senha);
-
-	$stmt->execute();
-
-	return (int) $db->lastInsertId();
-}
-
-function editar_usuario( int $id, string $nome, string $email, string $senha): bool 
-{
-	global $db;
-
-	$senha = password_hash( $senha, PASSWORD_DEFAULT);
-
-	$stmt = $db->prepare('	UPDATE 
-								usuario 
-							SET 
-								nome = :nome, email = :email, senha = :senha
-							WHERE
-								id = :id');
-
-	$stmt->bindParam(':id', $id);
-	$stmt->bindParam(':nome', $nome);
-	$stmt->bindParam(':email', $email);
-	$stmt->bindParam(':senha', $senha);
-
-	return $stmt->execute();
-}
-
-function apagar_usuario( int $id ): bool
-{
-	if ( is_numeric($id) ) {
-
-		global $db;
-
-		if ( $db->exec("DELETE FROM usuario WHERE id = $id") > 0 ) {
-
-			return true;
-
-		} else {
-
-			return false;
-		}
-
-	} else {
-
-		return false;
-	}
-}
+							foreach ($erros as $erro) {
+								echo $erro . '<br>';
+							}
+						}
+					}
+					?>
+					</font>
+					<br>
+					<br>
+					<label for="nome">Nome</label>
+					<input type="text" id="nome" name="nome" 
+							value="<?php echo $nome; ?>" required>
+					<br><br>
+					<label for="email">E-mail</label>
+					<input type="text" id="email" name="email"
+							value="<?php echo $email; ?>" required>
+					<br><br>
+					<label for="senha">Senha</label>
+					<input type="password" id="senha" name="senha" value="****" required>
+					<br>
+					<br>
+					<label for="conf_senha">Confirme a senha</label>
+					<input type="password" id="conf_senha" name="conf_senha" value="****" required>
+					<br><br>
+					<input type="hidden" name="id" value="<?php echo $_GET['editar']; ?>">
+					<input type="submit" name="gravar" value="Gravar">
+				</form>	
+			</td>
+		</tr>
+	</table>
+</center>
